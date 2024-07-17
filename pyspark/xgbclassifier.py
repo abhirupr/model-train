6493724a6d74f7146f97864a5b1f4de062f4c41e
abhirupr <abhirupr>
2024-07-17 13:07:06 +0000

    Fix 'no_progress_loss_custom' module

--- pyspark/xgbclassifier.py
+++ pyspark/xgbclassifier.py
@@ -4,6 +4,7 @@ from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import average_precision_score
 from hyperopt import hp, fmin, tpe, SparkTrials, STATUS_OK, space_eval
 from hyperopt.early_stop import no_progress_loss
+import logging
 
 class HyperOptXGBoostClassifier:
   def __init__(self, X_train, X_test, y_train, y_test):
@@ -84,20 +85,49 @@ class HyperOptXGBoostClassifier:
     self.best_params['min_child_weight'] = int(self.best_params['min_child_weight'])
     self.best_params['scale_pos_weight'] = int(self.best_params['scale_pos_weight'])
 
+  logger = logging.getLogger(__name__)
+
   def no_progress_loss_custom(iteration_stop_count=20, percent_increase=0.0):
+    """
+    Stop function that will stop after X iteration if the loss doesn't increase
+    Parameters
+    ----------
+    iteration_stop_count: int
+        search will stop if the loss doesn't improve after this number of iteration
+    percent_increase: float
+        allow this percentage of variation within iteration_stop_count.
+        Early stop will be triggered if the data didn't change for more than this number
+        after iteration_stop_count rounds
+    """
     def stop_fn(trials, best_loss=None, iteration_no_progress=0):
-      # print(trials.trials[-1])
+
       if trials.trials[-1]["result"]["status"] == "new":
-        return (False, [None, 0])
-      new_loss = trials.trials[-1]["result"]["loss"]
+        return False, [None, 0]
+
+      new_loss = trials.trials[len(trials.trials) - 1]["result"]["loss"]
+      
       if best_loss is None:
-          return False, [new_loss, iteration_no_progress + 1]
+        return False, [new_loss, iteration_no_progress]
+      
       best_loss_threshold = best_loss - abs(best_loss * (percent_increase / 100.0))
-      if new_loss < best_loss_threshold:
-          best_loss = new_loss
-          iteration_no_progress = 0
+      
+      # when fitting fail, pass or +1
+      if new_loss is None:
+        pass
+      elif new_loss < best_loss_threshold:
+        best_loss = new_loss
+        iteration_no_progress = 0
       else:
-          iteration_no_progress += 1
+        iteration_no_progress += 1
+        logger.debug(
+          "No progress made: %d iteration on %d. best_loss=%.2f, best_loss_threshold=%.2f, new_loss=%.2f" % (
+                iteration_no_progress,
+                iteration_stop_count,
+                best_loss,
+                best_loss_threshold,
+                new_loss,
+            )
+        )
 
       return (
           iteration_no_progress >= iteration_stop_count,
